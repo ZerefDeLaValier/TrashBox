0cc17213669d71d051edcf4676bc449ba0a1f619
ZerefDeLaValier <zerefdelavalier@gmail.com>
2019-09-26 14:06:04 +0500

    Release "C by n and m.py" v.1.0

--- Tests (WIP)/C by n and m.py
+++ Tests (WIP)/C by n and m.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import sys
 import math
+
 try:
+    if int(sys.argv[1]) > int(sys.argv[2]):
+        exit()
     fact = math.factorial(int(sys.argv[2])) / (math.factorial(int(sys.argv[1])) * math.factorial(int(sys.argv[2]) - int(sys.argv[1])))
 except(IndexError):
     m=int(input("Введите m: "))
     n=int(input("Введите n: "))
+    if m > n:
+        exit()
     fact = math.factorial(n) / (math.factorial(m) * math.factorial(n - m))
 print(fact)
 
