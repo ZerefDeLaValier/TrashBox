32a53566aedf0cf51be353089674274f3d20a72b
ZerefDeLaValier <zerefdelavalier@gmail.com>
2019-10-29 23:45:24 +0500

    Рассылка

--- HAHATUN/Vk-Bot/bot.py
+++ HAHATUN/Vk-Bot/bot.py
@@ -8,7 +8,7 @@ import base
 import config
 import threading
 
-
+id_list = []
 
 task = ["task1","task2","task3","task4","task5","task6","task7","task8","task9","task10"]
 treads = []
@@ -21,16 +21,22 @@ keyboard_subj = open("C:\\TrashBox\\HAHATUN\\Vk-Bot\\keyboard_subj.json", "r", e
 keyboard_yn = open("C:\\TrashBox\\HAHATUN\\Vk-Bot\\keyboard_yn.json", "r", encoding="UTF-8").read()
 keyboard_answr = open("C:\\TrashBox\\HAHATUN\\Vk-Bot\\keyboard_answr.json", "r", encoding="UTF-8").read()
 num = 0
+
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
         if event.text.lower() == 'начать':
                     vk.messages.send( 
                         user_id=event.user_id, random_id = random.randint(1, 2147483647),
                         message="Добро пожаловать в EduBot72! Напишите 'Помощь' для получения списка команд.", keyboard=keyboard_standart)
+                    
         elif event.text.lower() == 'помощь':
             vk.messages.send( 
                 user_id=event.user_id, random_id = random.randint(1, 2147483647),
                 message="Список команд: \n Предмет - выбор предмета для тестирования. \n Статистика - вывод вашей статистики по предметам. \n Рассылка - включение и отключение автоматической рассылки. \n Видео - видео-лекция для изучения материала по предмету. \n Тестирование - начало тестирования. \n Помощь - вывод списка команд." )
+            for id in id_list:
+                        vk.messages.send( 
+                                user_id=id, random_id = random.randint(1, 2147483647),
+                                message="Проверка рассылки. Лъягушка")
         elif event.text.lower() == 'предмет':
             i = 1
             vk.messages.send( 
@@ -51,18 +57,22 @@ for event in longpoll.listen():
         elif event.text.lower() == 'рассылка':
             vk.messages.send( 
                 user_id=event.user_id, random_id = random.randint(1, 2147483647),
-                message="Отключить авто-рассылку? \n Да \n Нет", keyboard=keyboard_yn)
+                message="Включить авто-рассылку? \n Да \n Нет", keyboard=keyboard_yn)
             for event in longpoll.listen():
                 if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
                     if event.text.lower() == 'да':
                         vk.messages.send(
                                         user_id=event.user_id, random_id = random.randint(1, 2147483647),
-                                        message="Рассылка отключена!", keyboard=keyboard_standart)
+                                        message="Рассылка включена!", keyboard=keyboard_standart)
+                        id_list.append(event.user_id)
+                        print(id_list)
                         break
                     elif event.text.lower() == 'нет':
                         vk.messages.send(
                                         user_id=event.user_id, random_id = random.randint(1, 2147483647),
-                                        message="Рассылка включена!", keyboard=keyboard_standart)
+                                        message="Рассылка отключена!", keyboard=keyboard_standart)
+                        id_list.remove(event.user_id)
+                        print(id_list)
                         break
         elif event.text.lower() == 'видео':
             vk.messages.send(
